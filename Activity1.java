cec32d3c66c1b94b3da2a599caf62838d2c755e7
sneham10 <sneha.muralidharan10@gamil.com>
2022-02-26 14:27:12 +0530

    done

--- Activity1.java
+++ Activity1.java
@@ -3,7 +3,9 @@ import java.util.*;
 class name{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
+System.out.println("Enter first name");
 String Firstname=sc.nextLine();
+System.out.println("Enter Last name");
 String Lastname=sc.nextLine();
 
 String F1=Firstname.substring(0,1);
